1c130436fffd741096603a7e7498932d57eb3f23
xiaofulihao <909831320@qq.com>
2023-10-08 10:20:41 +0800

    Site updated: 2023-10-08 10:20:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/ff05b5bf.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
